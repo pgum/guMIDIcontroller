590ac790e7c34925438195012511f5f79a6736ac
PJG <pjgumulka@gmail.com>
2020-11-27 17:59:18 +0100

    add functional from nonstd

--- gumidicontroller/guaction.h
+++ gumidicontroller/guaction.h
@@ -4,14 +4,14 @@ By creating own actions in namepsace Gu::Actions::<module>, they can be used to 
 */
 #ifndef guAction_H
 #define guAction_H
-//#include <functional>
+#include "functional-avr/nonstd.h"
 //#include <utility>
 #include <Arduino.h>
 
 namespace Gu::Actions {
 //a może da sie variable template na Callback zrobić?
-using Callback = void *();
-//using Callback = std::function<void()>;
+//using Callback = void *();
+using Callback = nonstd::function<void()>;
 
 struct Action { 
   Callback callback;
@@ -19,7 +19,6 @@ struct Action {
   //TODO: jakbym to chciał przerobić na operator String(...){}? to jak?
   //std::initializer_list<Q> takie coś gdzieś? https://en.cppreference.com/w/cpp/language/list_initialization
   //bo struct nie może mieć metod w środku, tak?
-  String toString(){ return String(name); }
 };
 
 //error: could not convert '{<lambda closure object>Gu::Actions::None()::<lambda()>{}, "---"}' from '<brace-enclosed initializer list>' to 'Gu::Actions::Action'
